aaf38eae144f3de46862f373c34f2eac9c5b9bd0
akshit0201 <akshit0201@gmail.com>
2022-03-11 07:33:55 +0530

    Created counter contract

---
+++ contracts/Counter.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Counter {
+    uint private counter;
+    function count() public returns (uint) {
+        counter++;
+        console.log("counter", counter);
+        return counter;
+    }
+}
