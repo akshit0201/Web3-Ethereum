d84353c9b3b4a9d3348131eee1ee467d99e65ea1
akshit0201 <akshit0201@gmail.com>
2022-03-12 19:33:16 +0530

    Added random stats gen

--- contracts/Hero.sol
+++ contracts/Hero.sol
@@ -4,10 +4,34 @@ contract Hero {
     enum Class {Mage,Barb,Heal}
 
     mapping(address => uint[]) hadd;
+
+    function generateRandom() public view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
+    }
 function geth() public view returns (uint[] memory){
 return hadd[msg.sender];
 }
     function cH(Class cl) public payable{
         require(msg.value >= 0.05 ether,"Insufficient funds");
+        uint[] stats = uint[](5);
+        stats[0] = 2;
+        stats[1] = 7;
+        stats[2] = 12;
+        stats[3] = 17;
+        stats[4] = 22;
+
+        uint l = 5;
+        uint hero = uint (cl);
+
+        do{
+            uint pos = generateRandom()%l;
+            uint val = generateRandom()%(13+l) +1;
+
+            hero |= val<<stats[pos];
+            l--;
+            stats[pos] = stats[l];
+        }
+        while(l>0);
+        hadd[msg.sender].push(hero);
     }
 }
